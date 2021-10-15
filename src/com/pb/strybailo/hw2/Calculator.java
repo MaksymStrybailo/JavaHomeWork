8a57a631398a0416ffecb8a6cb70a2022fc037f7
Maxstr75 <91918033+Maxstr75@users.noreply.github.com>
2021-10-15 06:45:13 +0300

    init commit

--- src/com/pb/strybailo/hw2/Calculator.java
+++ src/com/pb/strybailo/hw2/Calculator.java
@@ -25,7 +25,6 @@ public class Calculator {
                         }
                     default:
                         System.out.println("Неверная операция");
-
                     }
                 }
     }
--- src/com/pb/strybailo/hw2/Interval.java
+++ src/com/pb/strybailo/hw2/Interval.java
@@ -1,5 +1,19 @@
 package com.pb.strybailo.hw2;
 
-public class Interval {
+import java.util.Scanner;
 
+public class Interval {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Input first number: ");
+        int leftOperand = in.nextInt();
+
+        System.out.println("Input second number: ");
+        int rightOperand = in.nextInt();
+
+        System.out.println("Operator: ");
+        String operator = in.next();
+    }
 }
