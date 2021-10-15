c0cf3d0423d4dfd4250be24bc33748a05b3fc536
Maxstr75 <91918033+Maxstr75@users.noreply.github.com>
2021-10-15 16:09:56 +0300

    init commit

--- src/com/pb/strybailo/hw2/Calculator.java
+++ src/com/pb/strybailo/hw2/Calculator.java
@@ -1,24 +1,29 @@
 package com.pb.strybailo.hw2;
 
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
-        int x = 9;
-        int y = 0;
+        Scanner scan = new Scanner(System.in);
+        int x ;
+        int y ;
         String sign = "+";// + - * /
         System.out.println("Введите значение x:");
+        x = scan.nextInt();
         System.out.println("Введите значение y:");
+        y = scan.nextInt();
         switch (sign) {
             case "+":
-                System.out.println("Выполните" + (x + y));
+                System.out.println( + (x + y));
                 break;
             case "-":
-                System.out.println("Выполните" + (x - y));
+                System.out.println( + (x - y));
                 break;
             case "*":
-                System.out.println("Выполните" + (x * y));
+                System.out.println( + (x * y));
                 break;
             case "/":
-                System.out.println("Выполните" + (x / y));
+                System.out.println( + (x / y));
               if (y == 0) {
                   System.out.println("Ошибка.На ноль делить нельзя");
                   break;
